5762ed53da87db10d421dfb89e50b61e1520c6eb
Brett Connolly <brett84c@gmail.com>
2022-09-07 23:01:09 -0400

    fixes to properly load assets

--- DynamicCropPricing/ModEntry.cs
+++ DynamicCropPricing/ModEntry.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
@@ -9,8 +10,9 @@ using StardewValley;
 namespace DynamicCrops
 {
     /// <summary>The mod entry point.</summary>
-    public class ModEntry : Mod
+    public class ModEntry : Mod, IAssetLoader
     {
+        public string balanceMode = "realistic";
         /*********
         ** Public methods
         *********/
@@ -19,38 +21,65 @@ namespace DynamicCrops
         public override void Entry(IModHelper helper)
         {
             helper.Events.GameLoop.SaveCreated += OnSaveCreation;
-            helper.Events.GameLoop.Saving += OnSave;
+            helper.Events.Content.AssetRequested += this.OnAssetRequested;
+
         }
 
         private void OnSaveCreation(object sender, SaveCreatedEventArgs e)
         {
             Monitor.Log($"{Game1.player.Name}'s game has loaded...", LogLevel.Debug);
 
-            var objectModel = this.Helper.Data.ReadSaveData<ModData>("RFObjectData") ?? new ModData();
-            objectModel.CropData.Add("299", "5 5 5 5/summer/39/300/-1/1/false/false/false");
-            this.Helper.Data.WriteSaveData("RFObjectData", objectModel);
-
-            Monitor.Log(objectModel.ToString(), LogLevel.Debug);
             Monitor.Log("... DynamicCrops save file data created", LogLevel.Debug);
         }
 
-        private void OnSave(object sender, SavingEventArgs e)
+        private void OnAssetRequested(object sender, AssetRequestedEventArgs e)
         {
-            throw new NotImplementedException();
-        }
-
-        private void GetModeData(string mode)
-        {
-            var cropAndObjectData = ModData.getRealisticData();
-               
-            switch (mode.ToLower())
+            Monitor.Log($"{balanceMode} mode data loading...", LogLevel.Debug);
+            if (e.Name.IsEquivalentTo("TileSheets/crops"))
             {
-                case "even more realistic":
-                    cropAndObjectData = ModData.getEvenMoreRealisticData();
-                    break;
-                case "dynamic":
-                    cropAndObjectData = ModData.getLightweightData();
-                    break;
+                Monitor.Log("accesing crop tilesheet...");
+                e.LoadFromModFile<Texture2D>("assets/crops.png", AssetLoadPriority.Medium);
+                Monitor.Log("crop spritesheet loaded");
+            }
+            if (e.Name.IsEquivalentTo("Data/Crops"))
+            {
+                Monitor.Log("accessing crop data...", LogLevel.Debug);
+                switch (balanceMode)
+                {
+                    case "realistic":
+                        loadModeData(e, "assets/realistic-crop-data.json");
+                        break;
+                    case "moreRealistic":
+                        loadModeData(e, "assets/more-realistic-crop-data.json");
+                        break;
+                }
+                Monitor.Log("crop data loaded!", LogLevel.Debug);
+            }
+            if (e.Name.IsEquivalentTo("Data/ObjectInformation"))
+            {
+                Monitor.Log("accessing object data...", LogLevel.Debug);
+                switch (balanceMode)
+                {
+                    case "realistic":
+                        loadModeData(e, "assets/realistic-object-data.json");
+                        break;
+                    case "moreRealistic":
+                        loadModeData(e, "assets/more-realistic-object-data.json");
+                        break;
+                    case "lightweight":
+                        loadModeData(e, "assets/lightweight-object-data.json");
+                        break;
+                }
+                Monitor.Log("object data loaded!", LogLevel.Debug);
+            }
+
+            void loadModeData(AssetRequestedEventArgs e, string filepath)
+            {
+                e.Edit(asset =>
+                {
+                    var model = Helper.ModContent.Load<Dictionary<int, string>>("assets/realistic-object-data.json");
+                    foreach (var (key, value) in model) model[key] = value;
+                });
             }
         }
     }
