2a9ce2d134061bc8a6da7bc0eab0f0f88c84aa06
Brett Connolly <brett84c@gmail.com>
2022-09-28 17:13:45 -0400

    updated Fine, I'll Go Make My Own Seeds to custom mod

---
+++ FineIWillGoMakeMyOwnSeeds/ModConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DynamicCrops
+{
+    public class ModConfig
+    {
+        public bool harderSeedMaker { get; set; } = true;
+        public bool inflateSeedPrices { get; set; } = true;
+    }
+}
+
