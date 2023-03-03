b44cb6504f0df51891781d22fda46937d3b42f40
Brett Connolly <brett84c@gmail.com>
2023-03-03 18:29:30 -0500

    added crop-2-ore processor

--- WaterBalloon/ModEntry.cs
+++ WaterBalloon/ModEntry.cs
@@ -1,13 +1,11 @@
 ﻿
 
-using System.Collections.Generic;
 using System.IO;
 using Microsoft.Xna.Framework;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
 using StardewValley.TerrainFeatures;
-using xTile.Dimensions;
 
 namespace WaterBalloon
 {
@@ -17,9 +15,9 @@ namespace WaterBalloon
         void LoadAssets(string path);
     }
     /// <summary>The mod entry point.</summary>
-    public class ModEntry : StardewModdingAPI.Mod
+    public class ModEntry : Mod
     {
-        private IJsonAssetsApi JsonAssets;
+        private IJsonAssetsApi? JsonAssets;
         private int WaterBalloonID = -1;
         /*********
         ** Public methods
@@ -55,7 +53,7 @@ namespace WaterBalloon
         private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
         {
             //if activating the item and Water Balloon item exists water 3 tiles out from the player
-            if ((e.Button == SButton.MouseLeft || e.Button == SButton.ControllerA) && (Game1.player.CurrentItem.Name.Equals("Water Balloon") && WaterBalloonID != -1))
+            if ((e.Button == SButton.MouseLeft || e.Button == SButton.ControllerA) && Game1.player.CurrentItem?.Name == "Water Balloon" && WaterBalloonID != -1)
             {
                 const int radius = 2;
                 GameLocation location = Game1.currentLocation;
